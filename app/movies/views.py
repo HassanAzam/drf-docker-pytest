4125f20c0a5d2bfbccec70b01230bd6119f227aa
Hassan Azam <hassanazam@live.com>
2022-03-21 03:07:52 +0500

    isort formatting.

--- app/movies/views.py
+++ app/movies/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
 from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.shortcuts import render
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serializers import MovieSerializer
 from .models import Movie
-
+from .serializers import MovieSerializer
 
 # Create your views here.
 
